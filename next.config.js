f71157aaf79099dc3b48070f31a71b57f1754e4e
Berke Guleryuz <berkeguleryuz@hotmail.com>
2023-07-02 22:27:45 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["images.pexels.com", "www.pexels.com"],
       },
+      experimental: {
+        appDir: false,
+      },
 }
 
 module.exports = nextConfig;
