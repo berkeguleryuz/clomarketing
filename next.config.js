6dcc14f2017359dae7be423e05a9ac6e127c1542
Berke Guleryuz <berkeguleryuz@hotmail.com>
2023-07-02 22:44:10 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first")
+
 const nextConfig = {
     images: {
         domains: ["images.pexels.com", "www.pexels.com"],
       },
+      
 }
 
 module.exports = nextConfig;
