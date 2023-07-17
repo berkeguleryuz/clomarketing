8dd4849432b3d4bff5b9ee5e4047628abdf7af33
Berke Guleryuz <berkeguleryuz@hotmail.com>
2023-07-17 22:42:44 +0300

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -10,32 +10,30 @@ export const metadata = {
 
 const Contact = () => {
   return (
-<body>
-  <div className={styles.container}>
-    <h1 className={styles.title}>Lets Start!</h1>
-    <div className={styles.content}>
-      <div className={styles.imgContainer}>
-        <Image 
-        src="/1.svg"
-        alt=""
-        fill={true}
-        className={styles.image} 
-        />
-      </div>
-      <form className={styles.form}>
-        <input type="text" placeholder="Name" className={styles.input} />
-        <input type="text" placeholder="E-mail" className={styles.input} />
-        <textarea 
-          className={styles.textarea} 
-          placeholder = "Your Message" 
-          cols="20" 
-          rows="5">
+    <div className={styles.container}>
+      <h1 className={styles.title}>Lets Start!</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/1.svg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="text" placeholder="E-mail" className={styles.input} />
+          <textarea
+            className={styles.textarea}
+            placeholder="Your Message"
+            cols="20"
+            rows="5">
           </textarea>
-          <Button url="#" text="Send"/>
-      </form>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
     </div>
-  </div>
-</body>
   );
 }
 
