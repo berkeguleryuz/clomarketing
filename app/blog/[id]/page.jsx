be524ea07f393dcc7aec44bb520f24a2bbb0bbf4
Berke Guleryuz <berkeguleryuz@hotmail.com>
2023-07-17 22:46:42 +0300

    update

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation'
 
 
 async function getData(id) {
-  const res = await fetch(`http://127.0.0.1::3000/api/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
     next: { revalidate: 10 }
   });
